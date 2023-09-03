7931b942602a777ad2e1c6518e10054417eea124
sranucci <sranucci@itba.edu.ar>
2023-09-03 11:27:40 -0300

    ADD: products are now createable via API

--- API/app.js
+++ API/app.js
@@ -27,19 +27,33 @@ app.get('/users/:id', (req, res) => {
 app.post('/users', (req, res) => {
     //todo
     const {nro_cliente, nombre, apellido, direccion, activo} = req.body;
-    /*
-    nro_cliente int         not null
-        primary key,
-    nombre      varchar(45) not null,
-    apellido    varchar(45) not null,
-    direccion   varchar(45) not null,
-    activo      smallint    not null
-    */
+
     executeQuery('INSERT INTO E01_CLIENTE VALUES (?,?,?,?,?)',nro_cliente, nombre, apellido, direccion, activo)
     .then(response => res.status(200).end())
     .catch(err => console.log(err));
-    
+        
 });
+
+app.post("/products", (req, res) => {
+    /*
+    create table E01_PRODUCTO
+(
+    codigo_producto int         not null
+        primary key,
+    marca           varchar(45) not null,
+    nombre          varchar(45) not null,
+    descripcion     varchar(45) not null,
+    precio          float       not null,
+    stock           int         not null
+);
+
+    */
+   const { codigo_producto, marca, nombre, descripcion, precio, stock} = req.body;
+   executeQuery('INSERT INTO E01_PRODUCTO values (?,?,?,?,?,?)', codigo_producto, marca, nombre, descripcion, precio, stock)
+   .then(response => res.status(200).end())
+   .catch(err => console.log(err));
+})
+
 
 app.use((req, res) => {
     res.status(404).end()
