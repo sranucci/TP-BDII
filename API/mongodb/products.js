35b87f751715aa21ae22e8f85f0d9f110206e973
sranucci <sranucci@itba.edu.ar>
2023-10-13 12:23:15 -0300

    ADD: CRU operations on products

--- API/mongodb/products.js
+++ API/mongodb/products.js
@@ -1,16 +1,48 @@
 const express = require('express');
+const {getdb} = require("./datasource");
 const router = express.Router();
 
 //todo api methods
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const { codigo_producto, marca, nombre, descripcion, precio, stock} = req.body;
+    const db = await getdb();
+
+    try{
+        await db.collection("E01_PRODUCTO").insertOne({codigo_producto,marca,nombre,descripcion,precio,stock});
+    } catch (err){
+        res.status(500).end();
+    }
+    res.status(201).end();
+
 });
 
 
-router.put("/:id", (req,res) => {
+router.put("/:id", async (req,res) => {
     const oldId = parseInt(req.params.id);
+    const db = await getdb();
     const { codigo_producto, marca, nombre, descripcion, precio, stock} = req.body;
+    const update = {};
+    if (codigo_producto)
+        update.codigo_producto = codigo_producto;
+    if (marca)
+        update.marca = marca;
+    if (nombre)
+        update.nombre = nombre;
+    if (descripcion)
+        update.descripcion = descripcion;
+    if (precio)
+        update.precio = precio;
+    if (stock)
+        update.stock = stock;
+
+    try {
+        const {matchedCount} = await db.collection("E01_PRODUCTO").updateOne({codigo_producto : oldId}, {$set :update});
+        matchedCount === 1 ? res.status(200).end(): res.status(404).end();
+    } catch (err){
+        res.status(500).end();
+    }
+
 });
 
 module.exports = router;
--- API/mongodb/users.js
+++ API/mongodb/users.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {getdb}= require('./datasource')
 
-
-
-
 //todo error handling
 
 //todo api methods
