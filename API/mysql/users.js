f568cca7b78a07fa61b0376ece8c164e31abfb4b
Juan Ramiro Castro <juancastro@itba.edu.ar>
2023-11-17 20:01:54 -0300

    Refactor user methods with improved validation and error handling

--- API/mysql/users.js
+++ API/mysql/users.js
@@ -4,71 +4,156 @@ const {executeQuery} = require('./datasource') //todo
 
 router.get('/', (req, res) => {
     executeQuery('SELECT * FROM E01_CLIENTE')
-        .then( (result) => res.json(result) )
+        .then((result) => res.json(result))
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res, next) => {
     //todo
-    const {nro_cliente, nombre, apellido, direccion, activo} = req.body;
+    console.log(req.body)
+    let {nro_cliente, nombre, apellido, direccion, activo, telefonos} = req.body;
+
+    let setTelefonos;
 
     if (!nro_cliente || !nombre || !apellido || !direccion || !activo) {
-        return res.status(400).json({error: 'Missing required fields. Please provide values for nro_cliente, nombre, apellido, direccion, and activo.'});
+        console.log(nro_cliente, nombre, apellido, direccion, activo)
+        return res.status(400).json({error: 'Missing required fields. Please provide values for nro_cliente, nombre, apellido, direccion, and activo.'}).end();
     }
 
-    executeQuery('INSERT INTO E01_CLIENTE VALUES (?,?,?,?,?)',nro_cliente, nombre, apellido, direccion, activo)
-        .then(response => res.status(201).end())
-        .catch(err => {
-            console.log(err);
-            res.status(500).end();
-        });
-    
+    if (telefonos && Array.isArray(telefonos)) {
+        for (const telefono of telefonos) {
+            if (typeof telefono.codigo_area === 'undefined' ||
+                typeof telefono.nro_telefono === 'undefined' ||
+                typeof telefono.tipo === 'undefined') {
+                return res.status(400).json({
+                    error: 'Missing required fields for telefono. Please provide values for codigo_area, nro_telefono, and tipo.'
+                });
+            }
+        }
+        setTelefonos = telefonos;
+    } else if (!Array.isArray(telefonos)) {
+        setTelefonos = [];
+    }
+
+    try {
+        await executeQuery('INSERT INTO E01_CLIENTE VALUES (?,?,?,?,?)', nro_cliente, nombre, apellido, direccion, activo);
+        for (const telefono of setTelefonos) {
+            await executeQuery('INSERT INTO E01_TELEFONO VALUES (?,?,?,?)', telefono.codigo_area, telefono.nro_telefono, telefono.tipo[0], nro_cliente);
+
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).end();
+    }
+    res.status(201).end();
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     const {id} = req.params;
+    let response = {telefonos: []};
+    const result = await executeQuery('SELECT * FROM E01_CLIENTE WHERE nro_cliente = ?', parseInt(id));
+    if (result.length) {
+        response = result[0];
+    } else {
+        return res.status(404).json({error: 'The resource doesn\'t exist'}).end();
+    }
 
-    executeQuery('SELECT * FROM E01_CLIENTE WHERE nro_cliente = ?', id)
-        .then( (result) => {
+    executeQuery('SELECT * FROM E01_TELEFONO WHERE nro_cliente = ?', parseInt(id))
+        .then((result) => {
             if (!result.length)
-                next()
+                response.telefonos = [];
             else
-                res.json(result)
-        } )
+                response.telefonos = result;
+            res.json(response).status(200).end();
+        })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const {id} = req.params;
-    executeQuery('DELETE FROM E01_CLIENTE WHERE nro_cliente = ?', id)
-        .then(() => res.status(200).end())
-        .catch(err => console.log(err));
+    await executeQuery('DELETE FROM E01_TELEFONO WHERE nro_cliente = ?', parseInt(id));
+    try {
+        await executeQuery('DELETE FROM E01_CLIENTE WHERE nro_cliente = ?', parseInt(id))
+        res.status(200).end();
+    } catch (e) {
+        console.log(e);
+        res.status(500).end();
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const {id} = req.params;
-    const {nombre, apellido, direccion, activo} = req.body;
-    const string = "UPDATE E01_CLIENTE SET ";
+    let {nombre, apellido, direccion, activo, telefonos} = req.body;
+    let string = "UPDATE E01_CLIENTE SET ";
     const values = [];
+    let setTelefonos;
+
+    if (telefonos && Array.isArray(telefonos)) {
+        for (const telefono of telefonos) {
+            if (typeof telefono.codigo_area === 'undefined' ||
+                typeof telefono.nro_telefono === 'undefined' ||
+                typeof telefono.tipo === 'undefined') {
+                return res.status(400).json({
+                    error: 'Missing required fields for telefono. Please provide values for codigo_area, nro_telefono, and tipo.'
+                });
+            }
+        }
+        setTelefonos = telefonos;
+    } else {
+        setTelefonos = [];
+    }
+
+
     if (nombre) {
-        string.concat("nombre = ?, ");
+        if (values.length === 0)
+            string = string.concat("nombre = ? ");
+        else
+            string = string.concat(", nombre = ? ");
         values.push(nombre);
     }
     if (apellido) {
-        string.concat("apellido = ?, ");
+        if (values.length === 0)
+            string = string.concat("apellido = ? ");
+        else
+            string = string.concat(", apellido = ? ");
         values.push(apellido);
     }
     if (direccion) {
-        string.concat("direccion = ?, ");
+        if (values.length === 0)
+            string = string.concat("direccion = ? ");
+        else
+            string = string.concat(", direccion = ? ");
         values.push(direccion);
     }
     if (activo) {
-        string.concat("activo = ?, ");
+        if (values.length === 0)
+            string = string.concat("activo = ? ");
+        else
+            string = string.concat(", activo = ? ");
         values.push(activo);
     }
-    string.concat(" WHERE nro_cliente = ? ")
-    values.push(id);
-    executeQuery(string, ...values)
-        .then(response => res.status(200).end())
-        .catch(err => console.log(err));
+    string = string.concat(" WHERE nro_cliente = ? ")
+    values.push(parseInt(id));
+    try {
+        await executeQuery(string, ...values);
+    } catch (e) {
+        console.log(e);
+        res.status(500).end();
+    }
+
+    for (const telefono of setTelefonos) {
+        try {
+            const sqlResult = await executeQuery('SELECT * FROM E01_TELEFONO WHERE codigo_area = ? AND nro_telefono = ?', telefono.codigo_area, telefono.nro_telefono);
+            if (sqlResult.length) {
+                await executeQuery('UPDATE E01_TELEFONO SET tipo = ?, nro_cliente = ? WHERE codigo_area = ? AND nro_telefono = ?', telefono.tipo[0], parseInt(id), telefono.codigo_area, telefono.nro_telefono);
+            } else {
+                await executeQuery('INSERT INTO E01_TELEFONO VALUES (?,?,?,?)', telefono.codigo_area, telefono.nro_telefono, telefono.tipo[0], parseInt(id));
+            }
+        } catch (e) {
+            console.log(e);
+            res.status(500).end();
+        }
+    }
+
+    res.status(200).end();
 });
 
 module.exports = router
